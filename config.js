b20a3a881a28fbe020e440264140a8d1b90d1d5e
Hossam Elsabbagh <hossamelsabbagh770@gmail.com>
2026-06-25 20:44:56 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,2 @@
-// Supabase configuration
-// 1) Create a Supabase project
-// 2) Open Project Settings > API
-// 3) Copy Project URL and anon public key here
-
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://mnelaptowbttvzhgorcj.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_b2wpSxK_oawToLXZ3kJUZA_HOMkqpJr";
